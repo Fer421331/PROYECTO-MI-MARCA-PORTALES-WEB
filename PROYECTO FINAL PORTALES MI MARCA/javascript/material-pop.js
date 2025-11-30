e37ddd6a8d195424ddaf09434033b85c4a4181d1
Fer421331 <fernandogofu04@gmail.com>
2025-11-30 17:12:46 -0600

    Refactor modal functionality in material-pop.js

--- PROYECTO FINAL PORTALES MI MARCA/javascript/material-pop.js
+++ PROYECTO FINAL PORTALES MI MARCA/javascript/material-pop.js
@@ -3,7 +3,6 @@ const closeBtn = document.querySelector(".close");
 const tituloProducto = document.getElementById("titulo-producto");
 const formCotizar = document.getElementById("form-cotizar");
 
-// Abrir modal y cambiar el título
 document.querySelectorAll(".btn-cotizar").forEach(btn => {
     btn.addEventListener("click", () => {
         const producto = btn.getAttribute("data-producto");
@@ -12,24 +11,21 @@ document.querySelectorAll(".btn-cotizar").forEach(btn => {
     });
 });
 
-// Cerrar modal con la X
 closeBtn.addEventListener("click", () => {
     modal.style.display = "none";
 });
 
-// Cerrar modal haciendo clic afuera
 window.addEventListener("click", e => {
     if (e.target === modal) {
         modal.style.display = "none";
     }
 });
 
-// Evento de envío del formulario
 formCotizar.addEventListener("submit", (e) => {
-    e.preventDefault(); // Evita recargar
+    e.preventDefault(); 
 
     alert("Su cotización se envió con éxito ✔️");
 
-    formCotizar.reset();       // Limpia el formulario
-    modal.style.display = "none"; // Cierra el modal
+    formCotizar.reset();     
+    modal.style.display = "none"; 
 });
